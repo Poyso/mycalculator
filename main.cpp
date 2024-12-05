50ba2e76219a33568675ce8059641785950af64b
Poyso <costantini039@gmail.com>
2024-12-05 17:08:39 +0100

    clean up main and testing push in virtual machine

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/Graphics/Font.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/Text.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/VideoMode.hpp>
-#include <SFML/Window/Window.hpp>
 
 #include "mybutton.h"
 
@@ -15,8 +6,6 @@
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(600, 800), "My window");
-  // sf::RectangleShape rectangle(
-  //     sf::Vector2f(CALCULATOR_BUTTON_SIZE, CALCULATOR_BUTTON_SIZE));
   myButton button1 =
       createButton(0, 0, "1", sf::Color::White, sf::Color::Black,
                    CALCULATOR_BUTTON_SIZE, CALCULATOR_BUTTON_SIZE);
